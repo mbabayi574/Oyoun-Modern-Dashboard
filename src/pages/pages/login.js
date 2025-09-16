571aa6c87dc3981a83439160b3fb62d3a503f63c
Bobby <M.Babati574@Gmail.Com>
2025-09-16 09:38:07 +0330

    In The Name Of God

---
+++ src/pages/pages/login.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { AppSettings } from "./../../config/app-settings.js";
+
+function PagesLogin() {
+  const context = useContext(AppSettings);
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    context.setAppHeaderNone(true);
+    context.setAppSidebarNone(true);
+    context.setAppContentClass("p-0");
+
+    return function cleanUp() {
+      context.setAppHeaderNone(false);
+      context.setAppSidebarNone(false);
+      context.setAppContentClass("");
+    };
+
+    // eslint-disable-next-line
+  }, []);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    setRedirect(true);
+  }
+
+  if (redirect) {
+    return <Navigate to="/" />;
+  }
+  return (
+    <div className="login">
+      <div className="login-content">
+        <form onSubmit={handleSubmit}>
+          <h1 className="text-center">ورود</h1>
+          <div className="text-inverse text-opacity-50 text-center mb-4">
+            برای حفاظت از شما، لطفا هویت خود را تأیید کنید.
+          </div>
+          <div className="mb-3">
+            <label className="form-label">
+              آدرس ایمیل <span className="text-danger">*</span>
+            </label>
+            <input
+              type="text"
+              className="form-control form-control-lg bg-white bg-opacity-5"
+              placeholder=""
+            />
+          </div>
+          <div className="mb-3">
+            <div className="d-flex">
+              <label className="form-label">
+                رمزعبور <span className="text-danger">*</span>
+              </label>
+              <a
+                href="#/"
+                className="ms-auto text-inverse text-decoration-none text-opacity-50"
+              >
+                فراموشی رمزعبور؟
+              </a>
+            </div>
+            <input
+              type="password"
+              className="form-control form-control-lg bg-white bg-opacity-5"
+              placeholder=""
+            />
+          </div>
+          <div className="mb-3">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="customCheck1"
+              />
+              <label className="form-check-label" htmlFor="customCheck1">
+                من را به خاطر بسپار
+              </label>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-outline-theme btn-lg d-block w-100 fw-500 mb-3"
+          >
+            ورود
+          </button>
+          <div className="text-center text-inverse text-opacity-50">
+            حساب کاربری ندارید؟ <Link to="/pages/register">ثبت‌نام کنید</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default PagesLogin;
